e7ba016a4a79afc2409cc225add8ac2f1b067d11
Grosjean Thibaud <grosjeanthibaud@Grosjeans-MacBook-Air.local>
2024-12-05 16:50:27 +0100

    fixes update factory from drawers doesnt update product

--- src/components/FactoryOverseer.tsx
+++ src/components/FactoryOverseer.tsx
@@ -1,23 +1,27 @@
 'use client'
 
 import { useFormFields, useField, useDocumentEvents } from '@payloadcms/ui'
-import { useEffect, useRef, useCallback } from 'react'
+import { useEffect, useRef, useCallback, useState } from 'react'
 import { useFactoryDoc, useManageCollection } from '../context/FactoryContext'
 import { Field } from 'payload'
 
 export const FactoryOverseer: React.FC<{ path: string; field: Field }> = ({ path, field }) => {
   const factorySlug = field.admin?.components?.clientProps?.factory
-  const factoryId = useFormFields(([fields]) => fields?.factory?.value)
+  const factoryId = useFormFields(
+    ([fields]) => fields?.factory?.value?.value || fields?.factory?.value,
+  )
   const { value, setValue } = useField({ path })
   const { doc } = useFactoryDoc(factorySlug, factoryId || '')
   const manageCollection = useManageCollection(factorySlug)
-  const prevUpdateRef = useRef<string | null>(null)
-  console.log(doc)
-  const handleManageCollection = useCallback(() => {
+  const prevUpdateRef = useRef<any>({ updatedAt: '' })
+  const [isDocUpdated, setIsDocUpdated] = useState(false)
+
+  const handleManageCollection = useCallback(async () => {
     if (factorySlug) {
-      manageCollection()
+      await manageCollection()
+      setIsDocUpdated(true)
     }
-  }, [manageCollection])
+  }, [manageCollection, factorySlug])
 
   useEffect(() => {
     handleManageCollection()
@@ -26,17 +30,27 @@ export const FactoryOverseer: React.FC<{ path: string; field: Field }> = ({ path
   const { mostRecentUpdate } = useDocumentEvents()
 
   useEffect(() => {
-    if (
-      mostRecentUpdate?.entitySlug &&
-      mostRecentUpdate?.entitySlug === factorySlug &&
-      mostRecentUpdate !== prevUpdateRef.current
-    ) {
-      prevUpdateRef.current = mostRecentUpdate
-      if (mostRecentUpdate) {
-        handleManageCollection()
+    const updateDoc = async () => {
+      if (
+        mostRecentUpdate &&
+        mostRecentUpdate.entitySlug === factorySlug &&
+        mostRecentUpdate.updatedAt !== prevUpdateRef.current.updatedAt
+      ) {
+        prevUpdateRef.current = mostRecentUpdate
+        await handleManageCollection()
       }
     }
+
+    updateDoc()
   }, [mostRecentUpdate, handleManageCollection, factorySlug])
+
+  useEffect(() => {
+    if (isDocUpdated) {
+      // console.log('Updated doc:', doc)
+      setIsDocUpdated(false)
+      setValue(doc)
+    }
+  }, [isDocUpdated, doc])
 
   useEffect(() => {
     if (factoryId && value && factoryId !== value.id && doc) {
